c0c90ca45eeb830990b3200c81aee9c517050a19
ilinandre <ilinandre@ozon.ru>
2023-06-25 15:34:20 +0300

    upd::sass

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,14 @@
 import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: preprocess({
+		postcss: true,
+		scss: true
+	}),
 
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
@@ -13,6 +16,6 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
 	}
-};
+}
 
 export default config;
